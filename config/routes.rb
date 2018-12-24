37b1d2ae830bd86669da97533f8fa729e4aad616
eri <jta.e.m6@gmail.com>
2018-12-24 16:39:25 +0900

    ビュー作成用

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
-  resouces :items
+  get 'items/show' => 'items#show'
 end
